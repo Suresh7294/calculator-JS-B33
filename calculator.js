58c6175b27838888c83f1828a79c45d8194cbfe7
Suresh7294 <sureshsakthi653@gmail.com>
2025-07-10 09:48:42 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,23 @@
+
+    const display = document.getElementById('display');
+
+    function appendValue(val) {
+      if (display.innerText === '0') {
+        display.innerText = val;
+      } else {
+        display.innerText += val;
+      }
+    }
+
+    function clearDisplay() {
+      display.innerText = '0';
+    }
+
+    function calculate() {
+      try {
+        display.innerText = eval(display.innerText);
+      } catch {
+        display.innerText = 'Error';
+      }
+    }
+  
